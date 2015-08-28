5804dc2b0ef746f0bf2a270a6b9e254c9150221e
elvece <lvcifferello@gmail.com>
2015-08-28 11:24:50 -0600

    added iteration two, works successfully

--- main.js
+++ main.js
@@ -1,21 +1,46 @@
 var fs = require('fs');
 
+//iteration one
+// fs.readFile('files/start.txt', 'utf8', function (err, data) {
+//   var sentence = " ";
+//   var lines = data.split('\n');
+//   // console.log(lines);
+//   sentence += lines[0] + " ";
+
+//   fs.readFile('files/next.txt', 'utf8', function(err, data){
+//     var lines = data.split('\n');
+//     sentence += lines[0] + " ";
+
+//     fs.readFile('files/again.txt', 'utf8', function(err, data){
+//       var lines = data.split('\n');
+//       sentence += lines[0] + " ";
+//       console.log(sentence);
+
+//     });
+//   });
+// });
+
+//iteration two
 fs.readFile('files/start.txt', 'utf8', function (err, data) {
   var sentence = " ";
   var lines = data.split('\n');
-  // console.log(lines);
-
   sentence += lines[0] + " ";
+  var next = lines[1];
 
-  fs.readFile('files/next.txt', 'utf8', function(err, data){
-    var lines = data.split('\n');
+  fs.readFile(next, 'utf8', function(err, data){
     sentence += lines[0] + " ";
 
-    fs.readFile('files/again.txt', 'utf8', function(err, data){
-      var lines = data.split('\n');
+    fs.readFile(next, 'utf8', function(err, data){
       sentence += lines[0] + " ";
       console.log(sentence);
 
     });
   });
 });
+
+
+
+
+
+
+
